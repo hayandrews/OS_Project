b033dcde68cf753a206ec35e69e348eaab0326d8
Hayley Andrews <hayzaffre@gmail.com>
2018-05-15 15:45:59 -0400

    Init

---
+++ OS_Project.c
@@ -0,0 +1,18 @@
+//
+//  OS_Project.c
+//  
+//
+//  Created by Hayley Andrews on 5/15/18.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}
+
+Node holdQueue1 = Null;
+Node holdQueue2 = Null;
