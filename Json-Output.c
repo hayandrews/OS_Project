a9219a7727367fcc16688f723726f8c6a3558110
Hayley Andrews <hayzaffre@gmail.com>
2018-05-19 14:58:44 -0400

    JSON continued

--- Json-Output.c
+++ Json-Output.c
@@ -22,41 +22,73 @@ extern int cur_time;
 extern int avail_dev;
 
 
+void printQueueToFile(struct Job *queue){
+    if (queue == NULL) {
+        fputs("[], ",output);
+    } else{
+        struct Job *pointer = queue;
+        fputs("[",output);
+        while (pointer != NULL){
+            if (pointer->next != NULL){
+                fprintf(output, "%d, ", pointer->job_num);
+            } else {
+                fprintf(output, "%d], ", pointer->job_num);
+            }
+            pointer = pointer->next;
+            
+        }
+    }
+}
+
 void outputJSON(){
     output = fopen("JSONtest.json", "w");
     fputs("{",output);
+    
     fputs("\"readyq\": ",output);
-    fputs("[1, 5], ",output);
+    printQueueToFile(ready_queue);
+    
     fputs("\"current_time\": ",output);
     fprintf(output, "%d, ", cur_time);
-    //fputs("10, ",output);
+    
     fputs("\"total_memory\": ",output);
     fprintf(output, "%d, ", main_memory);
-    //fputs("200, ",output);
+    
     fputs("\"available_memory\": ",output);
     fprintf(output, "%d, ", avail_mem);
-    //fputs("0, ",output);
+    
     fputs("\"total_devices\": ",output);
     fprintf(output, "%d, ", serial_devices);
-    //fputs("12, ",output);
+    
+    //TODO - Currently Running
     fputs("\"running\": ",output);
     fputs("2, ",output);
+    
     fputs("\"submitq\": ",output);
-    fputs("[], ",output);
+    printQueueToFile(submit_queue);
+    //fputs("[], ",output);
+    
     fputs("\"holdq2\": ",output);
-    fputs("[], ",output);
+    printQueueToFile(hold_queue_2);
+    //fputs("[], ",output);
+    
+    //TODO - All Jobs
     fputs("\"job\": ",output);
     fputs("[{\"arrival_time\": 4, \"devices_allocated\": 0, \"id\": 2, \"remaining_time\": 9}, {\"arrival_time\": 3, \"devices_allocated\": 0, \"id\": 1, \"remaining_time\": 6}, {\"arrival_time\": 8, \"devices_allocated\": 0, \"id\": 5, \"remaining_time\": 4}, {\"arrival_time\": 5, \"id\": 3, \"remaining_time\": 4}], ",output);
+    
     fputs("\"holdq1\": ",output);
-    fputs("[3], ",output);
+    printQueueToFile(hold_queue_1);
+    //fputs("[3], ",output);
+    
     fputs("\"available_devices\": ",output);
     fprintf(output, "%d, ", avail_dev);
-    //fputs("12, ",output);
+    
     fputs("\"quantum\": ",output);
     fprintf(output, "%d, ", time_quantum);
-    //fputs("4, ",output);
+    
+    //TODO - Completed Jobs
     fputs("\"completeq\": ",output);
     fputs("[], ",output);
+    
     fputs("\"waitq\": ",output);
     fputs("[]",output);
     fputs("}",output);
